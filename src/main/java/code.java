6dc65ed56ca37d95168005ec132f46e66181220c
oneaqi <50888398+oneaqi@users.noreply.github.com>
2019-11-07 12:02:50 +0100

    Update code.java

--- src/main/java/code.java
+++ src/main/java/code.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
  *  * Servlet implementation class SimpleServlet
  *   */
 @WebServlet("/key")
-public class SimpleServlet extends HttpServlet {
+public class Code extends HttpServlet {
    private static final long serialVersionUID = 1L;
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
